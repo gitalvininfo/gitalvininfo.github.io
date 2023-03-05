9ec77f3c1b485fc7ec05cfe008c12520ccf74da1
Alvin Yanson <yansonalvin29@gmail.com>
2023-03-05 14:33:22 +0800

    contact form fix

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -5,8 +5,8 @@
 function validateForm(selector) {
     Array.from(document.querySelectorAll(selector)).forEach(item => {
         item.addEventListener('input', (e) => {
-            if(e.target.value === ''){
-            item.dataset.touched = false;
+            if (e.target.value === '') {
+                item.dataset.touched = false;
             }
         });
         item.addEventListener('invalid', () => {
@@ -23,7 +23,7 @@ validateForm('.js-form .form-field');
 var form = document.querySelector('.js-form');
 var formName = '.js-form';
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     submitForm(e, formName);
 });
 
@@ -33,23 +33,33 @@ function submitForm(e, formName) {
     var email = $(formName + ' .js-field-email').val();
     var message = $(formName + ' .js-field-message').val();
 
-    var formData = {
+    var params = {
         name: name,
         email: email,
         message: message
     };
 
-    $.ajax({
-        type: "POST",
-        url: 'mail.php',
-        data: formData,
-        success: function () {
-            console.log('success');
-            //...
-        },
-        error: function () {
-            console.log('error');
-            //...
-        }
-    });
+    const serviceID = "service_ot3aj0n";
+    const templateID = "template_qe2lsej";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            $("#alvin-contactform")[0].reset();
+            alert("Thank you for reaching out to me. I will respond to your message as soon as possible.")
+        })
+        .catch(err => console.log(err));
+
+    // $.ajax({
+    //     type: "POST",
+    //     url: 'mail.php',
+    //     data: formData,
+    //     success: function () {
+    //         console.log('success');
+    //         //...
+    //     },
+    //     error: function () {
+    //         console.log('error');
+    //         //...
+    //     }
+    // });
 }
